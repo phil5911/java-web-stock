cb4f7c72402cf272f0f81d633f3b2c246ab69f9a
nderhore <nathan@derhore.fr>
2024-05-10 19:12:45 +0200

    feat(project):init project

---
+++ src/main/java/fr/studi/stock/StockApplication.java
@@ -0,0 +1,13 @@
+package fr.studi.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockApplication.class, args);
+	}
+
+}
